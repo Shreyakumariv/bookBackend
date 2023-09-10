8257faf9052294a3723425588695c50b6aeb5028
Shreyakumariv <shreyakumariv@gmail.com>
2023-09-10 23:09:24 +0530

    m

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose")
 const router = require("./routes/userRoutes");
 const { blogRouter } = require("./routes/blog-routes");
 const cors = require("cors");
-
+require("dotenv").config()
+const port =process.env.PORT || 7000
 const app = express();
 app.use(cors());
 // http:/localhost:7000/api/user
@@ -13,6 +14,6 @@ app.use("/api/user", router)
 app.use("/api/blog", blogRouter)
 
 mongoose.connect("mongodb+srv://shreyakumariv:Amit123@cluster0.qvvpwnr.mongodb.net/?retryWrites=true&w=majority")
-    .then(() => app.listen(7000))
+    .then(() => app.listen(port))
     .then(() => console.log("connect the database"))
     .catch((err) => console.log("err"));
